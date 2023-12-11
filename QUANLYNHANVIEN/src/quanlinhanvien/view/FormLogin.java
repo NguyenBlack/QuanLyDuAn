db10d1075f98706391c12e477b6452f3f05e1dfa
NguyenBlack <nguyenvdph35455@fpt.edu.vn>
2023-12-11 12:00:49 +0700

    Sprint3

---
+++ QUANLYNHANVIEN/src/quanlinhanvien/view/FormLogin.java
@@ -0,0 +1,199 @@
+package quanlinhanvien.view;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+import quanlynhanvien.model.Logins;
+import quanlynhanvien.service.LoginService;
+
+public class FormLogin extends javax.swing.JFrame {
+
+    LoginService sevice = new LoginService();
+    FormQuanLyNhanVien f = new FormQuanLyNhanVien();
+
+    public FormLogin() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+    private boolean validates() {
+        if ("".equals(txtuser.getText())) {
+            JOptionPane.showMessageDialog(this, "bạn chưa nhập username");
+            return false;
+        }
+        if ("".equals(txtpass.getText())) {
+            JOptionPane.showMessageDialog(this, "bạn chưa nhập password");
+            return false;
+
+        }
+        return true;
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtuser = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        txtpass = new javax.swing.JPasswordField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Login");
+
+        jLabel2.setText("Username");
+
+        jLabel3.setText("Password");
+
+        jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Register");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(99, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(46, 46, 46)
+                        .addComponent(jButton2)
+                        .addGap(19, 19, 19))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(39, 39, 39)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtuser)
+                            .addComponent(txtpass))))
+                .addGap(83, 83, 83))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtuser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtpass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addContainerGap(38, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if (validates() == false) {
+            validate();
+        } else {
+            List<Logins> list = LoginService.CheckLogin();
+            int count = 0;
+            for (Logins logins : list) {
+                if (logins.getUser().equals(txtuser.getText()) && logins.getPass().equals(txtpass.getText())) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 1) {
+                JOptionPane.showMessageDialog(null, "Đăng nhập thành công");
+                f.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "Sai tài khoản hoặc mật khẩu");
+            }
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        if (validates() == false) {
+            validate();
+        } else {
+            List<Logins> list = LoginService.CheckLogin();
+            int count = 0;
+            for (Logins logins : list) {
+                if (logins.getUser().equals(txtuser.getText())) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                JOptionPane.showMessageDialog(null, "Tên tài khoản này đã có");
+            } else {
+                LoginService.register(txtuser.getText(), txtpass.getText());
+                JOptionPane.showMessageDialog(null, "Đăng ký thành công");
+            }
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FormLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FormLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FormLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FormLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPasswordField txtpass;
+    private javax.swing.JTextField txtuser;
+    // End of variables declaration//GEN-END:variables
+}
--- QUANLYNHANVIEN/src/quanlynhanvien/service/NhanVienService.java
+++ QUANLYNHANVIEN/src/quanlynhanvien/service/NhanVienService.java
@@ -11,10 +11,117 @@ import quanlynhanvien.model.NhanVien;
 public class NhanVienService {
 
     public static List loadData() {
-        List<NhanVien> listNhanvien = new ArrayList<>();
+        List<NhanVien> listNhanvien;
         try {
+            listNhanvien = new ArrayList<>();
             String sql = "Select * from Nhanvien";
-            Connection con = ConnectDB.getConnection();
+            Connection con = DBConnect.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Long id = rs.getLong(1);
+                String ma = rs.getString(2);
+                String ten = rs.getString(3);
+                String gioitinh = rs.getString(4);
+                int tuoi = rs.getInt(5);
+                int luong = rs.getInt(6);
+                String email = rs.getString(7);
+                String phone = rs.getString(8);
+                NhanVien nv = new NhanVien(id, ma, ten, gioitinh, tuoi, luong, email, phone);
+                listNhanvien.add(nv);
+            }
+            con.close();
+            return listNhanvien;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int Insert(NhanVien nv) {
+        try {
+            Connection con = DBConnect.getConnection();
+            String sql = "Insert into nhanvien values(?,?,?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nv.getMa());
+            ps.setString(2, nv.getTen());
+            ps.setString(3, nv.getGioitinh());
+            ps.setInt(4, nv.getTuoi());
+            ps.setInt(5, nv.getLuong());
+            ps.setString(6, nv.getEmail());
+            ps.setString(7, nv.getSdt());
+           return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static int deleteSV(String ma) {
+        String sql = "delete nhanvien\n"
+                + "where MaNV = ?";
+        try {
+            Connection con = DBConnect.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, ma);
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static int Update(String ma, NhanVien nv) {
+        String sql = "update nhanvien set hoten=?,gioitinh=?,tuoi=?,luong =?,email=?,sodienthoai=?\n"
+                + "where MaNV = ?;";
+        try {
+            Connection con = DBConnect.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nv.getTen());
+            ps.setString(2, nv.getGioitinh());
+            ps.setInt(3, nv.getTuoi());
+            ps.setInt(4, nv.getLuong());
+            ps.setString(5, nv.getEmail());
+            ps.setString(6, nv.getSdt());
+            ps.setString(7, ma);
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    public static List<NhanVien> Timtheoma(String ma){
+        try {
+            List<NhanVien> listNhanvien = new ArrayList<>();
+            String sql = "select * from nhanvien where Manv = ? ";
+            Connection con = DBConnect.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, ma);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {                
+                Long id = rs.getLong(1);
+                String ma1 = rs.getString(2);
+                String ten = rs.getString(3);
+                String gioitinh = rs.getString(4);
+                int tuoi = rs.getInt(5);
+                int luong = rs.getInt(6);
+                String email = rs.getString(7);
+                String phone = rs.getString(8);
+                NhanVien nv = new NhanVien(id, ma, ten, gioitinh, tuoi, luong, email, phone);
+                listNhanvien.add(nv);
+            }
+            return listNhanvien;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static List Sapxeptheoluong() {
+        List<NhanVien> listNhanvien;
+        try {
+            listNhanvien = new ArrayList<>();
+            String sql = "Select * from Nhanvien order by luong asc";
+            Connection con = DBConnect.getConnection();
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(sql);
             while (rs.next()) {
@@ -26,7 +133,7 @@ public class NhanVienService {
                 int luong = rs.getInt(6);
                 String email = rs.getString(7);
                 String phone = rs.getString(8);
-                NhanVien nv = new NhanVien(id,ma, ten, gioitinh, tuoi, luong, email, phone);
+                NhanVien nv = new NhanVien(id, ma, ten, gioitinh, tuoi, luong, email, phone);
                 listNhanvien.add(nv);
             }
             con.close();
@@ -34,24 +141,6 @@ public class NhanVienService {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-    }
-
-    public static void Add(String ma, String ten, String gioitinh, int tuoi, int luong, String email, String phone) {
-        try {
-            Connection con = ConnectDB.getConnection();
-            String sql = "Insert into nhanvien values(?,?,?,?,?,?,?)";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, ma);
-            ps.setString(2, ten);
-            ps.setString(3, gioitinh);
-            ps.setInt(4, tuoi);
-            ps.setInt(5, luong);
-            ps.setString(6, email);
-            ps.setString(7, phone);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
